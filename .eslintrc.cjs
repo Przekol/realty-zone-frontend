b95891a7821c56ce5f2bf997d469944987f32e5c
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-02-14 18:53:30 +0100

    Add automatic import sorting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,31 +6,44 @@ module.exports = {
     jest: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   overrides: [],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier', 'simple-import-sort', 'import'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    camelcase: "error",
-    "spaced-comment": "error",
-    quotes: ["error", "single"],
-    "no-duplicate-imports": "error",
+    'react/react-in-jsx-scope': 'off',
+    camelcase: 'error',
+    'spaced-comment': 'error',
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {},
+    },
+    react: {
+      version: 'detect',
     },
   },
 };
