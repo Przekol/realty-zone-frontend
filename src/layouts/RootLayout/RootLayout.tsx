9c97a7aea9e8eea17373478944efa9379067a7f8
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-26 19:16:29 +0200

    Remove Header Component

--- src/layouts/RootLayout/RootLayout.tsx
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Footer } from '../../components/Footer';
-import { Header } from '../../components/Header';
 import { Navigation } from '../../components/Navigation';
 
 export const RootLayout = () => {
   return (
     <>
       <Navigation />
-      <Header />
-      <main>
-        <Outlet />
-      </main>
+      <Outlet />
       <Footer />
     </>
   );
