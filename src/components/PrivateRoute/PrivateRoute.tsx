ac94025c2c7202a1af4b1069f21d9a208249c19e
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-27 12:44:37 +0200

    Add PrivateRoute

---
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { useAuth } from '../../context/Auth';
+import { ROUTES } from '../../data/routes';
+
+export const PrivateRoute = () => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to={ROUTES.signIn} />;
+  }
+  return <Outlet />;
+};
