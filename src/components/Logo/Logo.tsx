b4ccc6211c438750d3af65d69c9c05e18915436a
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-27 14:09:06 +0200

    Add a link to the logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,16 @@
 import { Box, Image } from '@chakra-ui/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { RealtyZoneLogo } from '../../assets';
+import { ROUTES } from '../../routes';
 
 export const Logo = () => {
   return (
     <Box w={40}>
-      <Image src={RealtyZoneLogo} />
+      <Link to={ROUTES.home}>
+        <Image src={RealtyZoneLogo} />
+      </Link>
     </Box>
   );
 };
