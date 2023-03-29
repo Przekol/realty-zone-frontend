139eea2bc278236d5d8eb8d428f29802aee939cb
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-29 16:26:49 +0200

    Add ErrorPage, SignUpAction,SuccessfulSignUp

--- src/routes/AppRoutes/AppRoutes.tsx
+++ src/routes/AppRoutes/AppRoutes.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 
-import { ForgetPassword, SignIn, SignUp } from '@pages/Auth';
+import { ForgetPassword, SignIn, SignUp, SuccessfulSignUp } from '@pages/Auth';
+import { ErrorPage } from '@pages/Error';
 import { Home } from '@pages/Home';
 import { AddOffer, MyAccount, Profile } from '@pages/MyAccount';
 import { Offers } from '@pages/Offers';
 import { ROUTES } from '@routes/routes';
 
 import { AuthLayout, AuthorizedLayout, OffersLayout, UnauthorizedLayout } from '../../layouts';
+import { SignUpAction } from '../../services/actions/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <UnauthorizedLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -34,8 +37,10 @@ const router = createBrowserRouter([
           {
             path: ROUTES.signUp,
             element: <SignUp />,
+            action: SignUpAction,
           },
           { path: ROUTES.forgetPassword, element: <ForgetPassword /> },
+          { path: ROUTES.successfulSignUp, element: <SuccessfulSignUp /> },
         ],
       },
       {
