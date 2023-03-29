616e0a0f02d424dc56c1cf9b4bbade98df69d965
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-29 10:03:26 +0200

    Implementation of the registration form

--- src/components/pages/Auth/SignUp/SignUp.tsx
+++ src/components/pages/Auth/SignUp/SignUp.tsx
@@ -1,44 +1,19 @@
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import { FormControl, FormLabel, Input, InputGroup, InputRightElement, Stack, Button } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { Stack } from '@chakra-ui/react';
+import React from 'react';
 
-import { AuthFormButton, AuthFormCard, AuthFormContainer, AuthFormLinks } from '@ui/auths';
+import { SignUpForm } from '@forms/auth/SignUpForm';
+import { AuthFormCard, AuthFormContainer, AuthFormLinks } from '@ui/auths';
 import { AuthFormCaption } from '@ui/captions';
 import { signUpLinks } from '@utils/data/links';
 
 export const SignUp = () => {
-  const [showPassword, setShowPassword] = useState(false);
-
   return (
     <AuthFormContainer>
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
         <AuthFormCaption title='Zarejestruj się' description='i ciesz się wszystkimi naszymi funkcjonalnościami' />
         <AuthFormCard>
-          <Stack spacing={4}>
-            <FormControl id='email' isRequired>
-              <FormLabel>Email</FormLabel>
-              <Input type='email' />
-            </FormControl>
-            <FormControl id='password' isRequired>
-              <FormLabel>Hasło</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h={'full'}>
-                  <Button variant={'ghost'} onClick={() => setShowPassword((showPassword) => !showPassword)}>
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <FormControl id='repeatPassword' isRequired>
-              <FormLabel>Powtórz hasło</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-              </InputGroup>
-            </FormControl>
-            <AuthFormButton>Zarejestruj się</AuthFormButton>
-            <AuthFormLinks links={signUpLinks} />
-          </Stack>
+          <SignUpForm />
+          <AuthFormLinks links={signUpLinks} />
         </AuthFormCard>
       </Stack>
     </AuthFormContainer>
