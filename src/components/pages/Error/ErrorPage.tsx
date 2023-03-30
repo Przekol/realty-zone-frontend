97af65070e9f9484c0c3a961065be5026bf64263
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-30 16:25:53 +0200

    Change ErrorPage

--- src/components/pages/Error/ErrorPage.tsx
+++ src/components/pages/Error/ErrorPage.tsx
@@ -1,43 +1,28 @@
-import { CloseIcon } from '@chakra-ui/icons';
-import { Box, Flex } from '@chakra-ui/react';
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
 
-import { ROUTES } from '@routes/routes';
+import { ContentWrapper } from '@base/ContentWrapper';
 import { Information } from '@ui/Information/Information';
 
 import { DefaultLayout } from '../../../layouts/DefaultLayout';
 
 export const ErrorPage = () => {
   const error = useRouteError() as any;
-  const title = error?.status === 404 ? 404 : error?.status;
-  const subTitle = error?.status === 404 ? 'Strona nie istnieje' : 'Wystąpił błąd';
+  const title = error?.status === 404 ? '404' : 'Wystąpił błąd';
+  const subTitle = error?.status === 404 ? 'Strona nie istnieje' : `Status błędu: ${error?.status}`;
+  const description = error?.status === 404 ? 'Wygląda na to, żę strona, której szukasz, nie istnieje' : error?.message;
   return (
     <DefaultLayout>
-      <Box textAlign='center' py={10} px={6}>
+      <ContentWrapper>
         <Information
           title={title}
           subTitle={subTitle}
-          content={error?.message}
-          to={ROUTES.home}
-          buttonText={'Powrót na stronę główną'}
-        >
-          <Box display='inline-block'>
-            <Flex
-              flexDirection='column'
-              justifyContent='center'
-              alignItems='center'
-              bg={'red.500'}
-              rounded={'50px'}
-              w={'55px'}
-              h={'55px'}
-              textAlign='center'
-            >
-              <CloseIcon boxSize={'20px'} color={'white'} />
-            </Flex>
-          </Box>
-        </Information>
-      </Box>{' '}
+          description={description}
+          buttonName={'Powrót'}
+          type={'error'}
+          to={-1}
+        />
+      </ContentWrapper>
     </DefaultLayout>
   );
 };
