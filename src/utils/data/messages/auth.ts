ecb9061b821537bfeec721a400effc6bac6e2c30
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-30 16:41:23 +0200

    Add messages

---
+++ src/utils/data/messages/auth.ts
@@ -0,0 +1,17 @@
+import { ROUTES } from '@routes/routes';
+
+import { MessageInformation } from '@frontendTypes';
+
+type AuthMessages = {
+  successfulSignUp: MessageInformation;
+  [key: string]: MessageInformation; // dowolne inne klucze również są dozwolone
+};
+export const authMessages: AuthMessages = {
+  successfulSignUp: {
+    title: 'Dziękujemy za rejestracje',
+    description: 'Sprawdź swoją skrzynkę odbiorczą i kliknij w link aktywacyjny, aby aktywować swoje konto.',
+    buttonName: 'Zaloguj się',
+    to: ROUTES.signIn,
+    type: 'success',
+  },
+};
