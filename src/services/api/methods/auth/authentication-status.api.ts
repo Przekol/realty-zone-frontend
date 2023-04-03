566692668d4d38f3f33585aa52c143d1953326d3
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-03 10:16:24 +0200

    Refactor AuthenticationStatusLoader

--- src/services/api/methods/auth/authentication-status.api.ts
+++ src/services/api/methods/auth/authentication-status.api.ts
@@ -1,17 +1,14 @@
-import { AuthenticatedStatusResponse, ClientApiResponse } from '@backendTypes';
+import { AuthenticatedStatusResponse } from '@backendTypes';
 
 import { ENDPOINTS } from '@services/api/methods/endpoints';
+import { handleUnauthorizedResponse } from '@services/api/methods/helpers';
 import { ApiServer } from '@services/api/utils/api-server';
 import { errorMessages } from '@utils/exceptions';
 
-export const getAuthenticationStatusApi = (
-  onErrorCallback: (
-    response: ClientApiResponse<AuthenticatedStatusResponse>,
-  ) => Promise<ClientApiResponse<AuthenticatedStatusResponse>>,
-) => {
+export const getAuthenticationStatusApi = () => {
   return ApiServer.get<AuthenticatedStatusResponse>({
     endpoint: ENDPOINTS.authenticationStatus,
     customErrorMessages: errorMessages,
-    onErrorCallback,
+    onErrorCallback: async (response) => handleUnauthorizedResponse(response),
   });
 };
