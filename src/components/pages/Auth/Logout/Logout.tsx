50ca362bfd7c3dde221a2652d061995266fa2eb9
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-03 10:22:21 +0200

    Refactor: Remove AuthContext

--- src/components/pages/Auth/Logout/Logout.tsx
+++ src/components/pages/Auth/Logout/Logout.tsx
@@ -1,31 +1,16 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useSubmit } from 'react-router-dom';
 
-import { useAuth } from '@context/auth';
 import { ROUTES } from '@routes/routes';
 
 export const Logout = () => {
-  const { state, dispatch } = useAuth();
   const submit = useSubmit();
   const navigate = useNavigate();
 
   useEffect(() => {
-    handleLogout();
+    submit(null, { method: 'post' });
+    navigate(ROUTES.home);
   }, []);
 
-  const handleLogout = async () => {
-    try {
-      dispatch({ type: 'SET_LOADING' });
-      await submit(null, { method: 'post' });
-      dispatch({ type: 'LOGOUT' });
-    } finally {
-      dispatch({ type: 'CLEAR_LOADING' });
-      navigate(ROUTES.home);
-    }
-  };
-
-  if (state.isLoading) {
-    return <div>Loading....</div>;
-  }
   return null;
 };
