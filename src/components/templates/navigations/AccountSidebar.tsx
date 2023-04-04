5a2d30210d775498f7943a9477bd823b44fc5613
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-04 13:22:21 +0200

    Add AccountSidebar component

---
+++ src/components/templates/navigations/AccountSidebar.tsx
@@ -0,0 +1,38 @@
+import { Drawer, DrawerContent } from '@chakra-ui/react';
+import React from 'react';
+
+import { SidebarContent } from '@ui/SidebarContent';
+import { sideBarLinks } from '@utils/data/links';
+
+import { UserDetails } from '@frontendTypes';
+
+import { SidebarMobileNavigation } from './SidebarMobileNavigation';
+
+interface Props {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+  userDetails?: UserDetails;
+}
+
+export const AccountSidebar = ({ isOpen, onOpen, onClose, userDetails }: Props) => {
+  return (
+    <>
+      <SidebarContent links={sideBarLinks} onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
+      <Drawer
+        autoFocus={false}
+        isOpen={isOpen}
+        placement='left'
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size='full'
+      >
+        <DrawerContent>
+          <SidebarContent links={sideBarLinks} onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      <SidebarMobileNavigation userDetails={userDetails} onOpen={onOpen} />
+    </>
+  );
+};
