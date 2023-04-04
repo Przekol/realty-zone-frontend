275f230ca39d8b27edfa793425548de33e5c3a71
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-04 13:22:40 +0200

    Implement the MyAccount component

--- src/components/pages/MyAccount/MyAccount/MyAccount.tsx
+++ src/components/pages/MyAccount/MyAccount/MyAccount.tsx
@@ -1,11 +1,27 @@
+import { Box, useColorModeValue, useDisclosure } from '@chakra-ui/react';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-export const MyAccount = () => (
-  <div>
-    <header>Moje konto</header>
-    <div>
-      <Outlet />
-    </div>
-  </div>
-);
+import { AccountSidebar } from '@templates/navigations/AccountSidebar';
+
+import { UserDetails } from '@frontendTypes';
+
+const topBarUserMenuData: UserDetails = {
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'john@example.com',
+  role: 'Admin',
+  src: 'https://bit.ly/sage-adebayo',
+};
+
+export const MyAccount = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Box minH='100vh' bg={useColorModeValue('gray.100', 'gray.900')}>
+      <AccountSidebar userDetails={topBarUserMenuData} isOpen={isOpen} onClose={onClose} onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p='4'>
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
