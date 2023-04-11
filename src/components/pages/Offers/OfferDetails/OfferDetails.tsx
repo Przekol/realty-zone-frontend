c8c7b852d452c1c230de219dbc8758b664cdf682
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-11 12:00:45 +0200

    Add OfferDetailsLoader

--- src/components/pages/Offers/OfferDetails/OfferDetails.tsx
+++ src/components/pages/Offers/OfferDetails/OfferDetails.tsx
@@ -1,5 +1,9 @@
+import { OneOfferResponse } from '@backendTypes';
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 export const OfferDetails = () => {
+  const offer = useLoaderData() as OneOfferResponse;
+  console.log(offer);
   return <div>szczegóły oferty</div>;
 };
--- src/routes/children-routes/unauthorized-routes/offers/OffersRoutes.tsx
+++ src/routes/children-routes/unauthorized-routes/offers/OffersRoutes.tsx
@@ -3,9 +3,9 @@ import { RouteObject } from 'react-router-dom';
 
 import { OfferDetails, Offers } from '@pages/Offers';
 import { ROUTES } from '@routes/routes';
-import { OffersListLoader } from '@services/loaders';
+import { OfferDetailsLoader, OffersListLoader } from '@services/loaders';
 
 export const offersRoutes: RouteObject[] = [
   { index: true, element: <Offers />, loader: OffersListLoader },
-  { path: ROUTES.offerDetails, element: <OfferDetails /> },
+  { path: ROUTES.offerDetails, element: <OfferDetails />, loader: OfferDetailsLoader },
 ];
