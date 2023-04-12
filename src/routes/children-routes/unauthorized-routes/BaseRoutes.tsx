1688938cbea4296820154057ad28b0beba6d94cc
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-12 11:28:36 +0200

    implementation of ComingSoon

--- src/routes/children-routes/unauthorized-routes/BaseRoutes.tsx
+++ src/routes/children-routes/unauthorized-routes/BaseRoutes.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { RouteObject } from 'react-router-dom';
 
 import { AuthLayout, OffersLayout } from '@layouts';
+import { About } from '@pages/About';
 import { AuthErrorPage } from '@pages/Auth/AuthErrorPage/AuthErrorPage';
+import { Blog } from '@pages/Blog';
+import { Contact } from '@pages/Contact';
 import { Home } from '@pages/Home';
 import { ROUTES } from '@routes/routes';
 
@@ -25,4 +28,7 @@ export const baseRoutes: RouteObject[] = [
     element: <OffersLayout />,
     children: offersRoutes,
   },
+  { path: ROUTES.contact, element: <Contact /> },
+  { path: ROUTES.about, element: <About /> },
+  { path: ROUTES.blog, element: <Blog /> },
 ];
